e79fa13a4914bab832682150ec43db65d8b95347
James Connell <jconnell@pulumi.com>
2024-10-17 08:25:00 -0700

    Initial implementation + actions

--- sdk/nodejs/index.ts
+++ sdk/nodejs/index.ts
@@ -5,31 +5,33 @@ import * as pulumi from "@pulumi/pulumi";
 import * as utilities from "./utilities";
 
 // Export members:
-export * from "./provider";
-export * from "./staticPage";
+export { ProviderArgs } from "./provider";
+export type Provider = import("./provider").Provider;
+export const Provider: typeof import("./provider").Provider = null as any;
+utilities.lazyLoad(exports, ["Provider"], () => require("./provider"));
 
-// Import resources to register:
-import { StaticPage } from "./staticPage";
+export { StackSettingsArgs } from "./stackSettings";
+export type StackSettings = import("./stackSettings").StackSettings;
+export const StackSettings: typeof import("./stackSettings").StackSettings = null as any;
+utilities.lazyLoad(exports, ["StackSettings"], () => require("./stackSettings"));
+
 
 const _module = {
     version: utilities.getVersion(),
     construct: (name: string, type: string, urn: string): pulumi.Resource => {
         switch (type) {
-            case "xyz:index:StaticPage":
-                return new StaticPage(name, <any>undefined, { urn })
+            case "stack-management:index:StackSettings":
+                return new StackSettings(name, <any>undefined, { urn })
             default:
                 throw new Error(`unknown resource type ${type}`);
         }
     },
 };
-pulumi.runtime.registerResourceModule("xyz", "index", _module)
-
-import { Provider } from "./provider";
-
-pulumi.runtime.registerResourcePackage("xyz", {
+pulumi.runtime.registerResourceModule("stack-management", "index", _module)
+pulumi.runtime.registerResourcePackage("stack-management", {
     version: utilities.getVersion(),
     constructProvider: (name: string, type: string, urn: string): pulumi.ProviderResource => {
-        if (type !== "pulumi:providers:xyz") {
+        if (type !== "pulumi:providers:stack-management") {
             throw new Error(`unknown provider type ${type}`);
         }
         return new Provider(name, <any>undefined, { urn });
